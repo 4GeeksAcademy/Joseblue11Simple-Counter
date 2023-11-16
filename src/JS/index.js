7a6cebb587960a8e0a3609c2af952b804b28be4d
Jose Antonio Tovar <136499634+Joseblue11@users.noreply.github.com>
2023-11-16 15:31:04 +0000

    simple couter

---
+++ src/JS/index.js
@@ -0,0 +1,22 @@
+//import react into the bundle
+import React from "react";
+import ReactDOM from "react-dom";
+
+// include your styles into the webpack bundle
+import "../styles/index.css";
+
+//import your own components
+import Home from "./component/home.jsx";
+import Counter from "./component/counter";
+
+//Programa timer
+let tiempo = 0;
+setInterval(() => {
+    tiempo++;
+    ReactDOM.render(<Counter segundos={tiempo}/>, document.querySelector("#app"))
+}, 1000)
+
+
+
+//render your react application
+ReactDOM.render(<Counter segundos={0}/>, document.querySelector("#app"));
